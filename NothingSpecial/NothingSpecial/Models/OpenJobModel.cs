cc9cd692f763fca1a09eaedb11ad0f3ba738e5ce
Matt <matt.w.rude@gmail.com>
2019-04-01 23:54:45 -0500

    Added the OpenJobs Database
    I build the OpenJobs database to store the ticket information. I used the entity framework instead of the traditional MySQL because its easier to write and well integrated + supported by ASP.NET. All that was added was the database, model C# file, DbContext C# file, and the connection string to the web.config file.

---
+++ NothingSpecial/NothingSpecial/Models/OpenJobModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NothingSpecial.Models
+{       
+    [Table("OpenJobs")]
+    public class OpenJobModel
+    {
+        // Setting up the class for the Open Jobs Database. This is used by the Entity Framework to build
+        // the Open Jobs Database, which will house all of the work orders data.
+
+        // Client data
+        [Key]
+        public int JobID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+        
+        // Tech data
+        public string TechNotes { get; set; }
+        public double Estimate { get; set; }
+        public double FinalCost { get; set; }
+        public bool WorkComplete { get; set; }
+        public string AssetModel { get; set; }
+        public string AssetType { get; set; }
+    }
+}
