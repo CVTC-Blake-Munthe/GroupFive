9ace4006ac3ac2fb904b8ef673cc55ca16deecf1
Matt <matt.w.rude@gmail.com>
2019-04-24 15:29:56 -0500

    Added service request controller tests
    Wrote unit tests for the service request controller. I will be writing the unit tests for the technician interface tonight 4/24.

---
+++ NothingSpecial/NothingSpecial.Tests/Controllers/ServiceRequestControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NothingSpecial.Controllers;
+using NothingSpecial;
+using System.Web.Mvc;
+using NothingSpecial.Models;
+
+namespace NothingSpecial.Tests.Controllers
+{
+    [TestClass]
+    public class ServiceRequestControllerTest
+    {
+        [TestMethod]
+        public void ServiceRequestForm()
+        {
+            // Arrange 
+            ServiceRequestController controller = new ServiceRequestController();
+
+            // Act
+            ViewResult result = controller.ServiceRequestForm() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void ServiceRequestDetails()
+        {
+            // Arrange 
+            ServiceRequestController controller = new ServiceRequestController();
+
+            OpenJobModel ojm = new OpenJobModel();
+
+            // Act
+            ViewResult result = controller.ServiceRequestDetails(ojm) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
