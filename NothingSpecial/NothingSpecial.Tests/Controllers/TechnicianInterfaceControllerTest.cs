2ab0c67a0e2c2df31f8103f22aaeadf4625e9fb9
Matt <matt.w.rude@gmail.com>
2019-05-01 13:17:53 -0500

    Added Tests for Controllers & Views
    I added some of the required tests for the controllers and views. I am using Moq and the default testing library that comes with ASP.NET to test the controllers.

---
+++ NothingSpecial/NothingSpecial.Tests/Controllers/TechnicianInterfaceControllerTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NothingSpecial.Controllers;
+using NothingSpecial;
+using System.Web.Mvc;
+using NothingSpecial.Models;
+using Moq;
+
+namespace NothingSpecial.Tests.Controllers
+{
+    [TestClass]
+    public class TechnicianInterfaceControllerTest
+    {
+
+        OpenJobModel ojm = new OpenJobModel();
+
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange 
+            TechnicianInterfaceController controller = new TechnicianInterfaceController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange 
+
+
+
+            // Act
+
+            // Assert
+           
+
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange 
+            TechnicianInterfaceController controller = new TechnicianInterfaceController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+            // Arrange 
+
+            // Act
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange 
+         
+
+            // Act
+          
+
+            // Assert
+
+
+        }
+
+    }
+}
